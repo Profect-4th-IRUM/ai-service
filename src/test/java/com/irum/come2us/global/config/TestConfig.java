949c29380f6dc7794a1a53b9d28419bfd88008fe
haeda0225 <haeda0225@gmail.com>
2025-11-11 13:14:59 +0900

    feature #4: 개별 마이크로 서비스 설정

--- src/test/java/com/irum/come2us/global/config/TestConfig.java
+++ src/test/java/com/irum/come2us/global/config/TestConfig.java
@@ -20,6 +20,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @TestConfiguration
+@Import(GlobalAutoConfiguration.class)
 public class TestConfig {
     @Bean
     public MemberService memberService() {
